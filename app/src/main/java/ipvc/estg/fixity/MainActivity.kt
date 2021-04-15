14f02e62df6b6cb77d7f76401885c114a76ecf4b
Diogo Mota <62253135+diogo-mota99@users.noreply.github.com>
2021-04-15 19:17:19 +0100

    Login Screen XML

--- app/src/main/java/ipvc/estg/fixity/MainActivity.kt
+++ app/src/main/java/ipvc/estg/fixity/MainActivity.kt
@@ -1,11 +1,22 @@
 package ipvc.estg.fixity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+//import estg.ipvc.fixity.viewModel.NoteListActivity
+import ipvc.estg.fixity.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageView>(R.id.btn_notes)
+
+        //button.setOnClickListener {
+            //val intent = Intent(this@MainActivity, NoteListActivity::class.java)
+            //startActivity(intent)
+        //}
     }
 }
